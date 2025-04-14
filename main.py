268da8973ccf168c4a6f30f1c65c77365a617e19
pinkpie306 <2376782120@qq.com>
2025-04-14 20:27:29 +0800

    解决冲突后的版本

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ def greet(name):
    print(f"Hello, {name}!")
 def goodbye(name):
     print(f"Goodbye, {name}!")
-greet("GitHub and  VSCode, Everyone")
+greet("GitHub and VSCode, Everyone")
 goodbye("GitHub")
